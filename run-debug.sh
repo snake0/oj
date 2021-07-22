f6792bc86611581ceb25c398196270382b68ec5d
snake0 <1260865816@qq.com>
2021-07-22 11:07:22 +0800

    add run_debug.sh

---
+++ run-debug.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#g++ -std=c++11 main.cpp
+
+#for i in {1..6} ; do
+#    ./a.out < testcase/$i.in > testcase/$i.out
+#done
+
+#rm a.out
+
+gcc -g -o matrix matrix.c -O0
+valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ./matrix
+
